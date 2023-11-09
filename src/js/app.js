2ea7ea970e359e18426080b227ec74937cc9175c
lifeofdream25 <lifeofdream2602@gmail.com>
2023-11-09 17:44:33 +0200

    Add more information block

--- src/js/app.js
+++ src/js/app.js
@@ -14,11 +14,26 @@ const productsMenuItems = document.querySelectorAll('.products__menu_item');
 productsMenuItems.forEach((item, index1) => {
     item.addEventListener('click', () =>{
         productsMenuItems.forEach((item2, index2) => {
-            if(!item2.classList.contains('.not__active') && index1 == index2){
+            if(!item2.classList.contains('.not__active') && index1 != index2){
                 item2.classList.add('not__active');
             }
             else{
                 item2.classList.remove('not__active');
+            }
+        })
+    })
+})
+
+const moreMenuItems = document.querySelectorAll('.more-inform__menu-item');
+moreMenuItems.forEach((item, index1) => {
+    item.addEventListener('click', () =>{
+        moreMenuItems.forEach((item2, index2) => {
+            if(!item2.classList.contains('item-active') && index1 == index2){
+                item2.classList.add('item-active');
+            }
+            else if(index1 != index2)
+            {
+                item2.classList.remove('item-active');
             }
         })
     })
