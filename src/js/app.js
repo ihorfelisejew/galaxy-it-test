1ecc0e305359d02715c5a97f83cccc555af008fa
lifeofdream25 <lifeofdream2602@gmail.com>
2023-11-09 18:04:38 +0200

    changed files

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,22 @@ new Swiper('.products__slider', {
     spaceBetween: 45,
 });
 
+const headerMenuItems = document.querySelectorAll('.top__menu-list .menu_item');
+headerMenuItems.forEach((item1, index1) => {
+    const a = headerMenuItems.querySelector('.menu__link');
+    a.addEventListener('click', () =>{
+        headerMenuItems.forEach((item2, index2) => {
+            if(!item2.classList.contains('item__active') && index1 == index2){
+                item2.classList.add('item__active');
+            }
+            else if(index1 != index2)
+            {
+                item2.classList.remove('item__active');
+            }
+        })
+    })
+})
+
 const productsMenuItems = document.querySelectorAll('.products__menu_item');
 productsMenuItems.forEach((item, index1) => {
     item.addEventListener('click', () =>{
